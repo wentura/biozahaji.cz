3d03bafaea054a899319de499c72331bed254de5
Zbynek Svoboda <wentura17@seznam.cz>
2024-04-06 20:00:47 +0200

    carousel

---
+++ components/carouselBanner.jsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+
+const CarouselBanner = ({ images }) => {
+  // Settings for the slider
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 2500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  };
+
+  return (
+    <Slider {...settings} className="mx-auto w-full">
+      {images.map((image, index) => (
+        <div key={index}>
+          <img src={image} alt={`Slide ${index}`} />
+        </div>
+      ))}
+    </Slider>
+  );
+};
+
+export default CarouselBanner;
--- components/hero.jsx
+++ components/hero.jsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
 import React from "react";
+import CarouselBanner from "./carouselBanner.jsx";
 import HeaderNav from "./header-nav";
+
+const images = ["1.png", "2.png", "3.png"];
 
 export default function Hero() {
   return (
     <div className="mx-auto max-w-screen-2xl px-2 md:px-0	">
       <HeaderNav />
+      <CarouselBanner images={images} />
 
-      <section className="flex flex-col items-center hero-background">
+      {/* <section className="flex flex-col items-center hero-background">
         <div className="flex max-w-xl flex-col items-center pb-16 pt-8 text-center lg:pb-48 lg:pt-32">
           <img
             src="https://res.cloudinary.com/dsqrbvyhy/image/upload/v1712328176/biozahaji/logo1_dxk9bz.webp"
@@ -17,7 +21,7 @@ export default function Hero() {
             alt="logo"
           />
         </div>
-      </section>
+      </section> */}
     </div>
   );
 }
